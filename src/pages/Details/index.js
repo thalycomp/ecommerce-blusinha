cecbfa885906433571bb66864d77b23ffcb7cc91
Thalyta Alencar <thalycomp@gmail.com>
2020-05-17 10:45:16 -0300

    page details estilizada

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,46 +1,112 @@
 import React from 'react';
-import Checkbox from '../../components/Checkbox/index';
-
-import {
-    Container,
-    ProductDetails,
-    ProductConteiner,
-    ProductDescription,
-} from './styles';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { Container, ProductDetails, InfoProducts, ButtonsNav, DetailsContainer, SizeTable } from './styles';
 
 function Details() {
     return (
+        <>
+        <DetailsContainer>
         <Container>
+            <div>
+                <img
+                    src="https://t-static.dafiti.com.br/Ubg3fw2vDGCPyWHG0SbSfhNBrQA=/fit-in/427x620/static.dafiti.com.br/p/fatal-camiseta-fatal-logo-preta-2837-7570015-1-product.jpg"
+                    alt="Blusa preta"
+                />
+            </div>
             <ProductDetails>
-                <div className="">
-                    <img
-                        src="https://t-static.dafiti.com.br/Ubg3fw2vDGCPyWHG0SbSfhNBrQA=/fit-in/427x620/static.dafiti.com.br/p/fatal-camiseta-fatal-logo-preta-2837-7570015-1-product.jpg"
-                        alt="Blusa preta"
-                    />
+                <div>
+                    <span>CÓD: 123</span>
+                    <strong>Blusa preta muito bonita</strong>
+                    <p>
+                        Blusa preta produzida pela Blusinha para elegantes, 100%
+                        em algodão.
+                    </p>
                 </div>
-                <ProductConteiner>
-                    <ProductDescription>
-                        <strong>Camiseta básica confortável preta</strong>
-                        <p>
-                            Blusa básica da marca Derifante feita 100% de
-                            algodão.
-                        </p>
-                        <span>CÓD: 1234</span>
+                <strong>R$ 59,90</strong>
+                <div>
+                    <input type="checkbox" id="tamP" name="tamP" value="tamP" />
+                    <label htmlFor="tamP"> P </label>
 
-                        <div>
-                            <strong>R$ 59,90</strong>
-                            <span> 10 x de 5,99 sem juros</span>
-                        </div>
-                    </ProductDescription>
-                    <div>
-                        <Checkbox name="coisa" value="P" label="P" />
-                        <Checkbox name="coisa" value="M" label="M" />
-                        <Checkbox name="coisa" value="G" label="G" />
-                        <Checkbox name="coisa" value="GG" label="GG" />
-                    </div>
-                </ProductConteiner>
+                    <input type="checkbox" id="tamM" name="tamM" value="tamM" />
+                    <label htmlFor="tamM"> M </label>
+
+                    <input type="checkbox" id="tamG" name="tamG" value="tamG" />
+                    <label htmlFor="tamG"> G </label>
+
+                    <input
+                        type="checkbox"
+                        id="tamGG"
+                        name="tamGG"
+                        value="tamGG"
+                    />
+                    <label htmlFor="tamGG"> GG </label>
+                </div>
             </ProductDetails>
         </Container>
+        <ButtonsNav>
+                    <div>
+                        <button type="button" to="/">
+                            <IoIosArrowBack size={20} color="#fff" />
+                            CONTINUAR COMPRANDO
+                        </button>
+                        <button type="button" to="/cart">
+                            FINALIZAR COMPRA
+                            <IoIosArrowForward size={20} color="#fff" />
+                        </button>
+                    </div>
+        </ButtonsNav>
+        </DetailsContainer>
+        <InfoProducts>
+            <strong>Medidas</strong>
+            <div>
+                <SizeTable>
+                    <thead>
+                        <tr>
+                            <th />
+                            <th> Altura </th>
+                            <th> Largura </th>
+                            <th> Manga </th>
+                            <th> Circunfência </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> P </td>
+                            <td> 68 </td>
+                            <td> 49 </td>
+                            <td> 18 </td>
+                            <td> 36 </td>
+                        </tr>
+                        <tr>
+                            <td> M </td>
+                            <td> 70 </td>
+                            <td> 52 </td>
+                            <td> 19 </td>
+                            <td> 38 </td>
+                        </tr>
+                        <tr>
+                            <td> G </td>
+                            <td> 73 </td>
+                            <td> 55 </td>
+                            <td> 20 </td>
+                            <td> 40 </td>
+                        </tr>
+                        <tr>
+                            <td> GG </td>
+                            <td> 76 </td>
+                            <td> 58 </td>
+                            <td> 21 </td>
+                            <td> 42 </td>
+                        </tr>
+                    </tbody>
+                </SizeTable>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quibusdam architecto voluptates autem nostrum quia laudantium odio expedita quo repudiandae, nulla esse a, totam, iure ducimus aut vitae animi quis?
+                </p>
+                </div>
+
+        </InfoProducts>
+        </>
     );
 }
 
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,84 +1,135 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    margin-top: 60px;
-`;
 
-export const ProductDetails = styled.div`
+export const DetailsContainer = styled.main`
+    display: flex;
+    flex-direction: column;
     background-color: #fff;
     border-radius: 4px;
-    padding: 20px;
+    padding: 25px;
 
+    }
+`;
+
+export const Container = styled.div`
     display: flex;
-    justify-content: left;
-    align-items: flex-start;
-    flex-shrink: 1;
+    flex-direction: row;
+    justify-content: space-around;
 
     > div {
-        max-height: 300px;
-        max-width: 250px;
-
+        height: 300px;
         img {
-            margin-right: 80px;
             max-width: 100%;
             max-height: 100%;
-            border: 1px solid #eee;
-            padding: 0px 15px;
-            flex-shrink: 1;
-            border-radius: 4px;
         }
     }
 `;
 
-export const ProductConteiner = styled.div`
-    display: grid;
-    grid-template-rows: repeat(4, 1fr);
-    max-height: 500px;
-    max-width: 500px;
-    color: #333;
+
+
+
+export const ProductDetails = styled.section`
+    display: flex;
     flex-direction: column;
-    justify-content: space-between;
-    background-color: red;
+    justify-content: space-around;
+
+    > strong {
+        font-size: 30px;
+        color: #e73c7e;
+        border: 1px solid #e73c7e;
+        padding: 10px 20px;
+        text-align: center;
+        border-radius: 4px;
+    }
+
+    div {
+        strong {
+            font-size: 18px;
+            margin-bottom: 5px;
+        }
+        p {
+            font-size: 12px;
+        }
+        span {
+            display: block;
+            font-size: 10px;
+            color: #999;
+            margin-bottom: 4px;
+        }
+    }
+
+
+`;
+
+export const ButtonsNav = styled.section`
 
     div {
         display: flex;
+        justify-content: space-between;
+        border: 0;
+        border-top: 1px solid #eee;
 
-        input[type='checkbox'] {
-            cursor: pointer;
-            border: 4px solid red;
-            width: 15px;
-            height: 15px;
-        }
+        button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: linear-gradient(-75deg, #23a6d5, #e73c7e);
+            background-size: 400% 400% 400%;
 
-        input:checked {
-            background-color: red;
-        }
+            border: none;
+            color: #fff;
+            border-radius: 4px;
+            overflow: hidden;
+            margin-top: 10px;
+            padding: 12px;
+            font-weight: bold;
 
-        input:checked + label:before {
-            background-color: black;
+            transition: background 0.2s;
+
+            &:hover {
+                opacity: 0.7;
+            }
         }
     }
 `;
 
-export const ProductDescription = styled.div`
+
+export const InfoProducts = styled.div`
+    flex-wrap: wrap;
     display: flex;
     flex-direction: column;
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 25px;
+    margin-top: 40px;
+    align-items: center;
+    color: #999;
+    text-align: center;
 
-    strong {
-        font-size: 25px;
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: wrap;
+
+        p {
+            max-width: 300px;
+            margin-top: 5px;
+            text-align: justify;
+            font-size: 12px;
+            margin: 5px;
+        }
     }
 
-    p {
-        width: 100%;
-        text-align: justify;
-        color: #797979;
-        font-size: 14px;
-        margin-bottom: 4px;
-        margin-top: 4px;
+`;
+
+export const SizeTable = styled.table`
+    max-width: 200px;
+    text-align: center;
+
+    thead th {
+        padding: 4px;
+        border-bottom: 1px solid #eee;
     }
 
-    span {
-        font-size: 12px;
-        justify-items: flex-end;
-    }
 `;
