ea51a2baa752ca37b6ca213b21e007d1b6c9005e
Thalyta Alencar <thalycomp@gmail.com>
2020-05-21 17:12:19 -0300

    configurando reactotron

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Routes from './router';
+
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 
+import './config/ReactotronConfig';
+
 import GlobalStyled from './styles/global';
+
 import store from './store/index';
 
 function App() {
